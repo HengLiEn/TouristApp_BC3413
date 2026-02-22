d3d3a2f2be654d65ec5615d7837df2bb523e9b99
felicianathania <feli0055@e.ntu.edu.sg>
2026-02-22 17:12:36 +0800

    testing2

--- Felicia Project.py
+++ Felicia Project.py
@@ -3,12 +3,12 @@ import pandas as pd
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
-# 1️⃣ Load the CSV files
+# Load the CSV files
 menu_items = pd.read_csv("menu_items (1).csv")
 stalls = pd.read_csv("stalls.csv")
 hawker_centers = pd.read_csv("hawker_centers.csv")
 
-# 2️⃣ Merge menu_items with stalls using 'stall_id'
+# Merge menu_items with stalls using 'stall_id'
 menu_stalls = pd.merge(
     menu_items,
     stalls,
@@ -16,7 +16,7 @@ menu_stalls = pd.merge(
     how="inner"
 )
 
-# 3️⃣ Merge the result with hawker_centers
+# Merge the result with hawker_centers
 # Using hawker_id (from stalls) and center_id (from hawker_centres)
 full_data = pd.merge(
     menu_stalls,
@@ -26,14 +26,16 @@ full_data = pd.merge(
     how="inner"
 )
 
-# 4️⃣ Display result
+# Display result
 print(full_data.head())
 
-# tourist input
+# tourists' input
 budget = float(input("Enter your budget: "))
 
 recommended = full_data[full_data["price"] <= budget]
 
 print(recommended[["stall_name", "center_name", "item_name", "price" ]])
+
+
 
 
